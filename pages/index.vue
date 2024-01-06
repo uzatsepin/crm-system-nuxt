96982402ac6689848baf83e82f1952196c59c7ac
Andrii Zatsepin <uzatsepin@gmail.com>
2024-01-06 19:02:59 +0200

    added gradient for status kanban

--- pages/index.vue
+++ pages/index.vue
@@ -6,6 +6,7 @@ import dayjs from "dayjs";
 import {useMutation} from "@tanstack/vue-query";
 import type {EnumStatus} from "~/types/deals.types";
 import {COLLECTION_DEALS, DB_ID} from "~/app.constants";
+import {generateColumnStyle} from "~/components/kanban/generateGradient";
 
 useSeoMeta({
     title: "Home | CRM System"
@@ -59,7 +60,7 @@ function handleDrop(targetColumn: IColumn) {
             <div class="grid grid-cols-5 gap-16">
                 <div v-for="(column, index) in data" :key="column.id" @dragover="handleDragOver"
                      @drop="() => handleDrop(column)">
-                    <div class="rounded bg-slate-700 py-1 px-5 mb-2 text-center">
+                    <div class="rounded bg-slate-700 py-1 px-5 mb-2 text-center" :style="generateColumnStyle(index, data?.length)">
                         {{ column.name }}
                     </div>
                     <KanbanCreateDeal :refetch="refetch" :status="column.id"/>
