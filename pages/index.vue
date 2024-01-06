b1a15304e7e05b29f6cfbf694c6f4a0784de0b97
Andrii Zatsepin <uzatsepin@gmail.com>
2024-01-06 18:56:48 +0200

    added func to drag and drop cards in crm system

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
-import type { ICard, IColumn } from "~/components/kanban/kanban.types";
+import type {ICard, IColumn} from "~/components/kanban/kanban.types";
 import {useKanbanQuery} from "~/components/kanban/useKanbanQuery";
 import {convertCurrency} from "~/utils/convertCurrency";
 import dayjs from "dayjs";
+import {useMutation} from "@tanstack/vue-query";
+import type {EnumStatus} from "~/types/deals.types";
+import {COLLECTION_DEALS, DB_ID} from "~/app.constants";
 
 useSeoMeta({
     title: "Home | CRM System"
@@ -11,7 +14,39 @@ useSeoMeta({
 const dragCardRef = ref<ICard | null>(null);
 const sourceColumnRef = ref<IColumn | null>(null)
 
-const { data, isLoading, refetch} = useKanbanQuery()
+const {data, isLoading, refetch} = useKanbanQuery()
+
+type TypeMutationVariables = {
+    docId: string,
+    status?: EnumStatus
+}
+
+const {mutate} = useMutation({
+    mutationKey: ['move card'],
+    mutationFn: ({docId, status}: TypeMutationVariables) => {
+        DB.updateDocument(DB_ID, COLLECTION_DEALS, docId, {
+            status
+        })
+    },
+    onSuccess: () => {
+        refetch()
+    }
+})
+
+function handleDragStart(card: ICard, column: IColumn) {
+    dragCardRef.value = card;
+    sourceColumnRef.value = column
+}
+
+function handleDragOver(event: DragEvent) {
+    event.preventDefault()
+}
+
+function handleDrop(targetColumn: IColumn) {
+    if (dragCardRef.value && sourceColumnRef.value) {
+        mutate({docId: dragCardRef.value.id, status: targetColumn.id})
+    }
+}
 
 
 </script>
@@ -22,23 +57,26 @@ const { data, isLoading, refetch} = useKanbanQuery()
         <div v-if="isLoading">Loading...</div>
         <div v-else>
             <div class="grid grid-cols-5 gap-16">
-                <div v-for="(column, index) in data" :key="column.id">
+                <div v-for="(column, index) in data" :key="column.id" @dragover="handleDragOver"
+                     @drop="() => handleDrop(column)">
                     <div class="rounded bg-slate-700 py-1 px-5 mb-2 text-center">
                         {{ column.name }}
                     </div>
                     <KanbanCreateDeal :refetch="refetch" :status="column.id"/>
-                    <div v-for="card in column.items" :key="card.id">
-                        <UiCard class="mb-5" draggable="true">
-                            <UiCardHeader role="button">
-                                <UiCardTitle>{{ card.name }}</UiCardTitle>
-                                <UiCardDescription class="mt-2 block">{{ convertCurrency(card.price) }}</UiCardDescription>
-                            </UiCardHeader>
-                            <UiCardContent class="text-xs">
-                                Компания: {{ card.companyName }}
-                            </UiCardContent>
-                            <UiCardFooter>{{ dayjs(card.$createdAt).format('DD MMMM YYYY') }}</UiCardFooter>
-                        </UiCard>
-                    </div>
+                    <UiCard v-for="card in column.items" :key="card.id" class="mb-5" draggable="true"
+                            @dragstart="() => handleDragStart(card, column)">
+                        <UiCardHeader role="button">
+                            <UiCardTitle>{{ card.name }}</UiCardTitle>
+                            <UiCardDescription class="mt-2 block">{{
+                                    convertCurrency(card.price)
+                                }}
+                            </UiCardDescription>
+                        </UiCardHeader>
+                        <UiCardContent class="text-xs">
+                            Компания: {{ card.companyName }}
+                        </UiCardContent>
+                        <UiCardFooter>{{ dayjs(card.$createdAt).format('DD MMMM YYYY') }}</UiCardFooter>
+                    </UiCard>
                 </div>
             </div>
         </div>
